2d05e709347823dcd708d1c13d3d6b0e84466707
AndrexMed <giovanniandrex@gmail.com>
2023-06-12 16:24:48 -0500

    Se trato de reemplazar este metodo ya que en la nueva version de spring es obsoleto, al implementar el nuevo metodo, arroja 403

--- api/src/main/java/com/voll/medic/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/com/voll/medic/api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.voll.medic.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,6 +23,18 @@ public class SecurityConfigurations {
                 .and()
                 .build();
     }
+
+    /*@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and().build();
+    }
+    Arroja 403
+    */
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
