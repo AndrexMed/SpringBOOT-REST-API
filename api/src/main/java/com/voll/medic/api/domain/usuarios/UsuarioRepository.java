e587bee7c1151a5f219100decaf83206d6bfd1fb
AndrexMed <giovanniandrex@gmail.com>
2023-06-12 10:34:16 -0500

    Repository Service

---
+++ api/src/main/java/com/voll/medic/api/domain/usuarios/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.voll.medic.api.domain.usuarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByUserName(String username);
+}
