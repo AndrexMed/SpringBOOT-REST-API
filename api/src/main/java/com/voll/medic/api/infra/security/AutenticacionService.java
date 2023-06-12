0add8c65b87962d90f3a6ff233ee4e2be9888b2f
AndrexMed <giovanniandrex@gmail.com>
2023-06-12 10:59:30 -0500

    Configuracion de seguridad

--- api/src/main/java/com/voll/medic/api/infra/security/AutenticacionService.java
+++ api/src/main/java/com/voll/medic/api/infra/security/AutenticacionService.java
@@ -14,6 +14,6 @@ public class AutenticacionService implements UserDetailsService {
     private UsuarioRepository usuarioRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usuarioRepository.findByUserName(username);
+        return usuarioRepository.findByLogin(username);
     }
 }
